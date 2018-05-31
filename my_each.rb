e15e253e3802cc431e7ea065a05361d49df1c341
Hillevi Eklöw <hillevieklow@gmail.com>
2018-05-31 20:08:44 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
 def my_each(collection) # put argument(s) here
+  i = 0
+  while i < collection.length
+    i += 1
+  end
   # code here
+end
+
+collection = [1, 2, 3, 4]
+my_each(collection) do |i|
+  puts i
 end
